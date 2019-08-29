1dd1c9db7d224f99356f985fd586d4a463e35639
Lauren Shultz <lshultz2020@gmail.com>
2019-08-28 22:06:58 -0500

    initial setup

---
+++ app/javascript/components/HomeProvider.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HomeProvider extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>HomeProvider Page</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HomeProvider
