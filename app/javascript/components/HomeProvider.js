3c819acc264dacfd5b41326e88d90dce075c5e72
“Lauren <larz2016@gmail.com>
2019-10-25 18:44:33 -0500

    Cleaned up code

--- app/javascript/components/HomeProvider.js
+++ app/javascript/components/HomeProvider.js
@@ -1,19 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { Provider, connect } from 'react-redux'
+import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
-// import dispatchActions from './dispatch'
-// import selectors from './selectors'
-import UserHomePage from './userHomePage/UserHomePage'
-import {
-  ADD_POST,
-    ADD_COMMENT,
-    SET_PAGE_FILTER,
-    PageFilters,
-    addPost
-} from '../actions';
-import { getVisiblePosts } from './selectors'
 import HomePage from './HomePage'
 
 const store = configureStore();
@@ -27,24 +16,5 @@ class HomeProvider extends React.Component {
     );
   }
 }
-
-// const mapStateToProps = state => {
-//   return {
-//     userPosts: getVisiblePosts(state)
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     onAddPost: body => {
-//       dispatch(addPost(body))
-//     }
-//   }
-// }
-
-// const HomePage = connect(
-//   mapStateToProps,
-//   mapDispatchToProps
-// )(HomeProviderBase);
 
 export default HomeProvider;
