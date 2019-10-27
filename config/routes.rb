649a532023f3d7ed96759b542139a3f801d0148a
Bohassian <jacob.nmi.lee@gmail.com>
2019-10-27 14:32:18 -0500

    Remove user routes in favor of devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'landing#index'
-  get 'user/:id', to: 'users#show'
-  get 'sign_up', to: 'users#sign_up'
-  get 'login', to: 'users#login'
 
-  resources :users
+  # resources :users
   resources :posts
 end
