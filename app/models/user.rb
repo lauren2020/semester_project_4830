5fcde17c1253a21e9ab1bf6ae07bad86d4c074a7
Bohassian <jacob.nmi.lee@gmail.com>
2019-10-27 14:48:30 -0500

    wraps up JournalComment relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :journal_posts, inverse_of: :user
+  has_many :journal_comments, inverse_of: :user
 
   has_many :message_boards, inverse_of: :user
   has_many :posts, inverse_of: :user
