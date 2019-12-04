f3c5a780b0610c2aea0addbbe9e801f44309d239
“Lauren <larz2016@gmail.com>
2019-12-04 13:53:05 -0600

    Added server url

--- app/javascript/components/ApiHelper.js
+++ app/javascript/components/ApiHelper.js
@@ -1,4 +1,4 @@
-const baseEndpoint = "http://localhost:4000/"
+const baseEndpoint = "http://ec2-52-201-243-68.compute-1.amazonaws.com/"
 export const ApiHelper = {
     CREATE_GROUP_ENDPOINT: baseEndpoint + "create_group",
     ADD_MEMBER_TO_GROUP_ENDPOINT: id => baseEndpoint + "groups/" + id + "/add_member",
